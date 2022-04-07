886a88082e116daf4e914929c2eeebdce6c4b8e1
Raimenezes <60903065+Raimenezes@users.noreply.github.com>
2022-04-07 00:13:35 -0300

    versao final
    tentei diminuir a quantidade de if na função score, levando-os para funções isoladas e menores, diminuindo a complexidade da função score

--- tennis.py
+++ tennis.py
@@ -30,64 +30,88 @@ class Game:
     def get_p2points(self):
         return self.__player2points
     
-    
-    def score(self):
+    def empatando():
         result = ""
-        if (self.get_player1points() == self.get_player2points() and self.get_player1points() < constante_three):
-            if (self.get_player1points()==constante_zero):
+        if (self.get_player1points()==constante_zero):
                 result = "Love"
-            if (self.get_player1points()==constante_one):
+        if (self.get_player1points()==constante_one):
                 result = "Fifteen"
-            if (self.get_player1points()==constante_two):
+        if (self.get_player1points()==constante_two):
                 result = "Thirty"
-            result += "-All"
-        if (self.get_player1points()==self.get_player2points() and self.get_player1points()>constante_two):
-            result = "Deuce"
-        
+            result += "-All"       
+        return result  
+    
+    def player1Vencendo():
         P1res = ""
         P2res = ""
-        if (self.get_player1points()> constante_zero and self.get_player2points()==constante_zero):
-            if (self.get_player1points()==constante_one):
-                P1res = "Fifteen"
-            if (self.get_player1points()==constante_two):
-                P1res = "Thirty"
-            if (self.get_player1points()==constante_three):
-                P1res = "Forty"
-            
-            P2res = "Love"
-            result = P1res + "-" + P2res
-        if (self.get_player2points() > constante_zero and self.get_player1points()==constante_zero):
-            if (self.get_player2points()==constante_one):
+        if (self.get_player1points()==constante_one):
+            P1res = "Fifteen"
+        if (self.get_player1points()==constante_two):
+            P1res = "Thirty"
+        if (self.get_player1points()==constante_three):
+            P1res = "Forty"
+                
+        P2res = "Love"
+        result = P1res + "-" + P2res
+        return result
+        
+     def player2Vencendo():
+        if (self.get_player2points()==constante_one):
                 P2res = "Fifteen"
-            if (self.get_player2points()==constante_two):
+        if (self.get_player2points()==constante_two):
                 P2res = "Thirty"
-            if (self.get_player2points()==constante_three):
+        if (self.get_player2points()==constante_three):
                 P2res = "Forty"
             
-            P1res = "Love"
-            result = P1res + "-" + P2res
+        P1res = "Love"
+        result = P1res + "-" + P2res
+        
+    def player1venceu():
+        if (self.get_player1points()==constante_two):
+                P1res="Thirty"
+        if (self.get_player1points()==constante_three):
+                P1res="Forty"
+        if (self.get_player2points()==constante_one):
+                P2res="Fifteen"
+        if (self.get_player2points()==constante_two):
+                P2res="Thirty"
+        result = P1res + "-" + P2res
+        
+    def player2venceu():
+        if (self.get_player2points()==constante_two):
+            P2res="Thirty"
+        if (self.get_player2points()==constante_three):
+            P2res="Forty"
+        if (self.get_player1points()==constante_one):
+            P1res="Fifteen"
+        if (self.get_player1points()==constante_two):
+            P1res="Thirty"
+        result = P1res + "-" + P2res
+    
+    def score(self):
+        
+        if (self.get_player1points() == self.get_player2points() and self.get_player1points() < constante_three):
+            self.empatando()
+            
+  
+        if (self.get_player1points()> constante_zero and self.get_player2points()==constante_zero):
+            self.player1Vencendo()  
+            
+          
+        if (self.get_player2points() > constante_zero and self.get_player1points()==constante_zero):
+            self.player2Vencendo()
         
         
         if (self.get_player1points()>self.get_player2points() and self.get_player1points() < constante_for):
-            if (self.get_player1points()==constante_two):
-                P1res="Thirty"
-            if (self.get_player1points()==constante_three):
-                P1res="Forty"
-            if (self.get_player2points()==constante_one):
-                P2res="Fifteen"
-            if (self.get_player2points()==constante_two):
-                P2res="Thirty"
-            result = P1res + "-" + P2res
+           self.player1venceu()
+        
+        
         if (self.get_player2points()>self.get_player1points() and self.get_player2points() < constante_for):
-            if (self.get_player2points()==constante_two):
-                P2res="Thirty"
-            if (self.get_player2points()==constante_three):
-                P2res="Forty"
-            if (self.get_player1points()==constante_one):
-                P1res="Fifteen"
-            if (self.get_player1points()==constante_two):
-                P1res="Thirty"
-            result = P1res + "-" + P2res
+            self.player2venceu()
+            
+            
+        if (self.get_player1points()==self.get_player2points() and self.get_player1points()>constante_two):
+            result = "Deuce"
         
         if (self.get_player1points() > self.get_player2points() and self.get_player2points() >= constante_three):
             result = "Advantage " + get_Player1Name()
