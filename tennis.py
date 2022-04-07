f53ae6da41e648f8a9d99552b9b4ecb35fb367c9
Raimenezes <60903065+Raimenezes@users.noreply.github.com>
2022-04-06 21:34:05 -0300

    Constantes
    criei constantes e substitui nas chamadas, eliminando "números mágicos"

--- tennis.py
+++ tennis.py
@@ -1,10 +1,16 @@
 # -*- coding: utf-8 -*-
+constante_zero = 0
+constante_one = 1
+constante_two = 2
+constante_three = 3
+constante_for = 4
+
 class Game:
     def __init__(self, player1Name, player2Name):
         self.player1Name = player1Name
         self.player2Name = player2Name
-        self.p1points = 0
-        self.p2points = 0
+        self.p1points = constante_zero
+        self.p2points = constante_zero
         
     def won_point(self, playerName):
         if playerName == self.player1Name:
@@ -14,71 +20,71 @@ class Game:
     
     def score(self):
         result = ""
-        if (self.p1points == self.p2points and self.p1points < 3):
-            if (self.p1points==0):
+        if (self.p1points == self.p2points and self.p1points < constante_three):
+            if (self.p1points==constante_zero):
                 result = "Love"
-            if (self.p1points==1):
+            if (self.p1points==constante_one):
                 result = "Fifteen"
-            if (self.p1points==2):
+            if (self.p1points==constante_two):
                 result = "Thirty"
             result += "-All"
-        if (self.p1points==self.p2points and self.p1points>2):
+        if (self.p1points==self.p2points and self.p1points>constante_two):
             result = "Deuce"
         
         P1res = ""
         P2res = ""
-        if (self.p1points > 0 and self.p2points==0):
-            if (self.p1points==1):
+        if (self.p1points > constante_zero and self.p2points==constante_zero):
+            if (self.p1points==constante_one):
                 P1res = "Fifteen"
-            if (self.p1points==2):
+            if (self.p1points==constante_two):
                 P1res = "Thirty"
-            if (self.p1points==3):
+            if (self.p1points==constante_three):
                 P1res = "Forty"
             
             P2res = "Love"
             result = P1res + "-" + P2res
-        if (self.p2points > 0 and self.p1points==0):
-            if (self.p2points==1):
+        if (self.p2points > constante_zero and self.p1points==constante_zero):
+            if (self.p2points==constante_one):
                 P2res = "Fifteen"
-            if (self.p2points==2):
+            if (self.p2points==constante_two):
                 P2res = "Thirty"
-            if (self.p2points==3):
+            if (self.p2points==constante_three):
                 P2res = "Forty"
             
             P1res = "Love"
             result = P1res + "-" + P2res
         
         
-        if (self.p1points>self.p2points and self.p1points < 4):
-            if (self.p1points==2):
+        if (self.p1points>self.p2points and self.p1points < constante_for):
+            if (self.p1points==constante_two):
                 P1res="Thirty"
-            if (self.p1points==3):
+            if (self.p1points==constante_three):
                 P1res="Forty"
-            if (self.p2points==1):
+            if (self.p2points==constante_one):
                 P2res="Fifteen"
-            if (self.p2points==2):
+            if (self.p2points==constante_two):
                 P2res="Thirty"
             result = P1res + "-" + P2res
-        if (self.p2points>self.p1points and self.p2points < 4):
-            if (self.p2points==2):
+        if (self.p2points>self.p1points and self.p2points < constante_for):
+            if (self.p2points==constante_two):
                 P2res="Thirty"
-            if (self.p2points==3):
+            if (self.p2points==constante_three):
                 P2res="Forty"
-            if (self.p1points==1):
+            if (self.p1points==constante_one):
                 P1res="Fifteen"
-            if (self.p1points==2):
+            if (self.p1points==constante_two):
                 P1res="Thirty"
             result = P1res + "-" + P2res
         
-        if (self.p1points > self.p2points and self.p2points >= 3):
+        if (self.p1points > self.p2points and self.p2points >= constante_three):
             result = "Advantage " + self.player1Name
         
-        if (self.p2points > self.p1points and self.p1points >= 3):
+        if (self.p2points > self.p1points and self.p1points >= constante_three):
             result = "Advantage " + self.player2Name
         
-        if (self.p1points>=4 and self.p2points>=0 and (self.p1points-self.p2points)>=2):
+        if (self.p1points>=constante_for and self.p2points>=constante_zero and (self.p1points-self.p2points)>=constante_two):
             result = "Win for " + self.player1Name
-        if (self.p2points>=4 and self.p1points>=0 and (self.p2points-self.p1points)>=2):
+        if (self.p2points>=constante_for and self.p1points>=constante_zero and (self.p2points-self.p1points)>=constante_two):
             result = "Win for " + self.player2Name
         return result
     
@@ -91,8 +97,8 @@ class Game:
             self.P2Score()
     
     def P1Score(self):
-        self.p1points +=1
+        self.p1points +=constante_one
     
     
     def P2Score(self):
-        self.p2points +=1
+        self.p2points +=constante_one
